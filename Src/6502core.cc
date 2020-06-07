0dd3d1b7298fed308f97c07266ff7e10dc85503d
Ian Rutt <i.c.rutt@gmail.com>
2020-06-07 13:35:22 +0100

    Fixed minor commenting warning

--- Src/6502core.cc
+++ Src/6502core.cc
@@ -615,7 +615,7 @@ INLINE static void JSRInstrHandler(int16 address) {
 	  *pcptr=0;
 	  fprintf(osclilog,"%s\n",pcbuf);
   }
-  /*if (ProgramCounter==0xffdd) {
+  if (ProgramCounter==0xffdd) {
 	  char errstr[250];
 	  sprintf(errstr,"OSFILE called\n");
 	  MessageBox(GETHWND,errstr,"BBC Emulator",MB_OKCANCEL|MB_ICONERROR);
